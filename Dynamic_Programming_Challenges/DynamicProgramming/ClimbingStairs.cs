3898ed2d91b26b46986628985fe3f6e34eed0074
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-10 19:49:09 -0400

    Tests for recursive function

---
+++ Dynamic_Programming_Challenges/DynamicProgramming/ClimbingStairs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    public static class ClimbingStairs
+    {
+        /*  You are climbing a staircase that has n steps. 
+         *  You can take the steps either 1 or 2 at a time. 
+         *  Calculate how many distinct ways you can climb to the top of the staircase.
+
+            Example
+            For n = 1, the output should be
+            climbingStairs(n) = 1;
+
+            For n = 2, the output should be
+            climbingStairs(n) = 2.
+
+            You can either climb 2 steps at once or climb 1 step two times.
+        */
+
+        public static int HowManyDistinctWaysToClimbStairs(int numberOfStairs)
+        {
+            if (numberOfStairs == 1)
+                return numberOfStairs;
+            if (numberOfStairs == 2)
+                return numberOfStairs;
+
+            return HowManyDistinctWaysToClimbStairs(numberOfStairs - 2) + HowManyDistinctWaysToClimbStairs(numberOfStairs - 1);
+        }
+    }
+}
---
+++ Dynamic_Programming_Challenges/DynamicProgrammingTests/Tests.cs
@@ -0,0 +1,85 @@
+﻿using DynamicProgramming;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgrammingTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public int numberOfStairs;
+        [Test]
+        public void StaircaseWithOneStep_ReturnOneDistinctWay()
+        {
+            numberOfStairs = 1;
+            int expected = 1;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaircaseWithTwoSteps_ReturnTwo()
+        {
+            numberOfStairs = 2;
+            int expected = 2;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaircaseWithThreeSteps_ReturnThree()
+        {
+            numberOfStairs = 3;
+            int expected = 3;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaircaseWithFourSteps_ReturnFive()
+        {
+            numberOfStairs = 4;
+            int expected = 5;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaircaseWithFive_ReturnEight()
+        {
+            numberOfStairs = 5;
+            int expected = 8;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaircaseWithSix_ReturnThirteen()
+        {
+            numberOfStairs = 6;
+            int expected = 13;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaircaseWithSeven_ReturnTwentyOne()
+        {
+            numberOfStairs = 7;
+            int expected = 21;
+            int actual = ClimbingStairs.HowManyDistinctWaysToClimbStairs(numberOfStairs);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
