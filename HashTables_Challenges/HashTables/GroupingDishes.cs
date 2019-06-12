037f133196c151d5bb05e7995f0c4456f58ab365
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-12 16:29:13 -0400

    Setup tests and class for new method

---
+++ HashTables_Challenges/HashTables/GroupingDishes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public class GroupingDishes
+    {
+        public string[][] GroupingDishesByIngredients(string[][] dishes)
+        {
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ HashTables_Challenges/HashTablesTests/GroupingDishesTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesTests
+{
+    [TestFixture]
+    public class GroupingDishesTests
+    {
+        [Test]
+        public void ForTwoDishesWithOneIngredientEach_ReturnIngredientWithTwoDishNames()
+        {
+
+        }
+    }
+}
