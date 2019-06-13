848ac586ae721b073aee889637f8180a3e45210b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-13 14:01:12 -0400

    Added Tests for adding dishes and ingredients to dictionary

--- HashTables_Challenges/HashTables/GroupingDishes.cs
+++ HashTables_Challenges/HashTables/GroupingDishes.cs
@@ -8,6 +8,63 @@ namespace HashTables
 {
     public class GroupingDishes
     {
+        public Dictionary<string, int> AddIngredientsAndCountsToDictionary(string[][] dishes)
+        {
+            Dictionary<string, int> IngredientCount = new Dictionary<string, int>();
+
+            for (int row = 0; row < dishes.Length; row++)
+            {
+                for (int col = 1; col < dishes[row].Length; col++)
+                {
+                    if (IngredientCount.ContainsKey(dishes[row][col]))
+                        IngredientCount[dishes[row][col]]++;
+                    else
+                        IngredientCount.Add(dishes[row][col], 1);
+                }
+            }
+
+            return IngredientCount;
+        }
+
+        
+        public Dictionary<string, List<string>> AddIngredientsAndTheirDishesToDictionary(string[][] dishes)
+        {
+            Dictionary<string, List<string>> IngredientsAndTheirDishes = new Dictionary<string, List<string>>();
+            /*
+            string dish;
+            for (int row = 0; row < dishes[0].Length; row++)
+            {
+                dish = dishes[row][0];
+                Console.WriteLine("dish = {0}", dish);
+            }
+            */
+            
+            for (int row = 0; row < dishes.Length; row++)
+            {
+                string dish = dishes[row][0];
+
+                for (int col = 0; col < dishes[row].Length; col++)
+                {
+                    
+                    if (IngredientsAndTheirDishes.ContainsKey(dishes[row][col]))
+                    {
+                        IngredientsAndTheirDishes[dishes[row][col]].Add(dishes[row][0]);
+
+                    }
+                        
+                    else
+                    {
+                        IngredientsAndTheirDishes.Add(dishes[row][col], new List<string> { dish });
+
+                    }
+
+                }
+            }
+            
+            return IngredientsAndTheirDishes;
+        }
+
+
         public string[][] GroupingDishesByIngredients(string[][] dishes)
         {
 
--- HashTables_Challenges/HashTablesTests/GroupingDishesTests.cs
+++ HashTables_Challenges/HashTablesTests/GroupingDishesTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using HashTables;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,94 @@ namespace HashTablesTests
     [TestFixture]
     public class GroupingDishesTests
     {
+        GroupingDishes dishesToMake = new GroupingDishes();
+        
+
         [Test]
-        public void ForTwoDishesWithOneIngredientEach_ReturnIngredientWithTwoDishNames()
+        public void ForTwoWithOneIngredientEach_AddToDictionaryAndReturnIt()
         {
+           string[][] dishes = {
+                new string[] { "Salad", "Tomato" },
+                new string [] { "Pizza", "Tomato" }
+            };
+
+            Dictionary<string, int> Ingredients = new Dictionary<string, int>()
+            {
+                { "Tomato", 2 }
+            };
+
+            foreach (var item in Ingredients)
+                Console.WriteLine("key: {0}, Value: {1}", item.Key, item.Value);
+
+            var expected = Ingredients;
+            var actual = dishesToMake.AddIngredientsAndCountsToDictionary(dishes);
+
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        [Test]
+        public void ForFourDishesWithFourIngredientsEach_AddToDictionaryAndReturnIt()
+        {
+            string[][] dishes = {
+                new string[] { "Salad", "Tomato", "Cucumber", "Salad", "Sauce" },
+                new string [] { "Pizza", "Tomato", "Sausage", "Sauce", "Dough" },
+                new string[] { "Quesadilla","Chicken","Cheese","Sauce" },
+                new string[] { "Sandwich", "Salad", "Bread", "Tomato", "Cheese" }
+            };
+
+            Dictionary<string, int> Ingredients = new Dictionary<string, int>()
+            {
+                { "Bread", 1},
+                { "Cheese", 2 },
+                { "Chicken", 1},
+                { "Cucumber", 1 },
+                { "Dough", 1},
+                { "Salad", 2 },
+                { "Sauce", 3 },
+                { "Sausage", 1},
+                { "Tomato", 3 }
+            };
+
+            foreach (var item in Ingredients)
+                Console.WriteLine("key: {0}, Value: {1}", item.Key, item.Value);
+
+            var expected = Ingredients;
+            var actual = dishesToMake.AddIngredientsAndCountsToDictionary(dishes);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void ForFourDishesWithFourIngredientsEach_AddIngredientsAsKeysAndListOfDishes_ReturnDict()
+        {
+            string[][] dishes = {
+                new string[] { "Salad", "Tomato", "Cucumber", "Salad", "Sauce" },
+                new string [] { "Pizza", "Tomato", "Sausage", "Sauce", "Dough" },
+                new string[] { "Quesadilla","Chicken","Cheese","Sauce" },
+                new string[] { "Sandwich", "Salad", "Bread", "Tomato", "Cheese" }
+            };
+
+            Dictionary<string, List<string>> Ingredients = new Dictionary<string, List<string>>();
+
+            Ingredients.Add("Bread", new List<string> { "Sandwich" });
+            Ingredients.Add("Cheese", new List<string> { "Quesadilla", "Sandwich" });
+            Ingredients.Add("Chicken", new List<string> { "Quesadilla" });
+            Ingredients.Add("Cucumber", new List<string> { "Salad" });
+            Ingredients.Add("Dough", new List<string> { "Pizza" });
+            Ingredients.Add("Salad", new List<string> { "Salad", "Sandwich" });
+            Ingredients.Add("Sauce", new List<string> { "Salad", "Quesadilla", "Sandwich" });
+            Ingredients.Add("Sausage", new List<string> { "Pizza" });
+            Ingredients.Add("Tomato", new List<string> { "Salad", "Pizza", "Sandwich" });
+
+            foreach (var item in Ingredients)
+                Console.WriteLine("key: {0}, Value: {1}", item.Key, item.Value);
+
+            var expected = Ingredients;
+            var actual = dishesToMake.AddIngredientsAndTheirDishesToDictionary(dishes);
+
+            Assert.AreEqual(expected, actual);
 
         }
     }
