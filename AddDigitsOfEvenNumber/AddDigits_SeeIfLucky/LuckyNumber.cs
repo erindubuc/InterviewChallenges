1a666123f25ea7a9f7f7706e8048c40f74756175
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-25 14:05:18 -0400

    Determines if sum of first half of a num's digits is equal to sum of second half of its digits

---
+++ AddDigitsOfEvenNumber/AddDigits_SeeIfLucky/LuckyNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDigits_SeeIfLucky
+{
+    public static class LuckyNumber
+    {
+        /*  Ticket numbers usually consist of an even number of digits. 
+         *  A ticket number is considered lucky if the sum of the first 
+         *  half of the digits is equal to the sum of the second half.
+
+            Given a ticket number n, determine if it's lucky or not.
+        */
+
+        public static int GetNumberOfDigits(int num)
+        {
+            return num.ToString().Length;
+        }
+
+        public static bool IsThisNumberLucky(int num)
+        {
+            int numOfDigits = GetNumberOfDigits(num);
+            int leftHalfSum = 0;
+            int rightHalfSum = 0;
+            string numberString = num.ToString();
+
+
+            for (int digitsToAdd = 0; digitsToAdd < numOfDigits/2; digitsToAdd++)
+            {
+                leftHalfSum += numberString[digitsToAdd];
+            }
+            for (int digitsToAdd = numOfDigits / 2; digitsToAdd < numberString.Length; digitsToAdd++)
+            {
+                rightHalfSum += numberString[digitsToAdd];
+            }
+            if (leftHalfSum == rightHalfSum)
+                return true;
+            else
+                return false;
+        }
+
+       
+
+    }
+}
---
+++ AddDigitsOfEvenNumber/AddDigits_SeeIfLuckyTests/LuckyNumberTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddDigits_SeeIfLucky;
+
+namespace AddDigits_SeeIfLuckyTests
+{
+    [TestFixture]
+    public class LuckyNumberTests
+    {
+        [Test]
+        public void IfNumberIsTwoDigits_Return2()
+        {
+            int num = 34;
+
+            var expected = 2;
+            var actual = LuckyNumber.GetNumberOfDigits(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNumberIsThreeDigits_Return3()
+        {
+            int num = 345;
+
+            var expected = 3;
+            var actual = LuckyNumber.GetNumberOfDigits(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNumIsTwoDigits_CheckIfTheyAreEqual_Equal_ReturnTrue()
+        {
+            int num = 33;
+
+            var expected = true;
+            var actual = LuckyNumber.IsThisNumberLucky(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNumIsTwoDigits_CheckIfTheyAreEqual_NotEqual_ReturnFalse()
+        {
+            int num = 34;
+
+            var expected = false;
+            var actual = LuckyNumber.IsThisNumberLucky(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNumIsFourDigits_CheckIfTheirSumIsEqual_NotEqual_ReturnFalse()
+        {
+            int num = 3435;
+
+            var expected = false;
+            var actual = LuckyNumber.IsThisNumberLucky(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
